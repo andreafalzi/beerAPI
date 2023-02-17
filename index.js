9251f04690e52a6f002d6975504536877c2158a0
andreafalzi <af@andreafalzi.dev>
2023-02-17 01:44:14 +0100

    fixed beer path + country single path git add .still problem with characters conversion

--- index.js
+++ index.js
@@ -5,76 +5,107 @@ const cheerio = require('cheerio');
 
 const app = express();
 
-const beersListRanker = [];
-const beersListWikiliq = [];
-const beersListEBG = [];
-
 //MULTIPLE SITE SCRAPPING
 //(keep in mind that you have to check that all site has the same tag or html element to get the right data)
 
-// const beerWebSites = [
-//   {
-//     name: 'ranker',
-//     url: 'https://www.ranker.com/list/top-beers-from-denmark/reference',
-//   },
-//   {
-//     name: 'wikiliq',
-//     url: 'https://wikiliq.org/best-beer/',
-//   },
-// ];
+const beers = [
+  {
+    country: 'denmark',
+    url: 'http://www.europeanbeerguide.net/denbrew.htm',
+  },
+  {
+    country: 'sweden',
+    url: 'http://www.europeanbeerguide.net/swedbrew.htm',
+  },
+  {
+    country: 'belgium',
+    url: 'http://www.europeanbeerguide.net/belgbrew.htm',
+  },
+  {
+    country: 'spain',
+    url: 'http://www.europeanbeerguide.net/spanbrew.htm',
+  },
+];
 
-// const beersList = [];
+const beersList = [];
 
-// beerWebSites.forEach(site => {
-//   axios.get(site.url).then(response => {
-//         const html = response.data;
-//         const $ = cheerio.load(html);
+beers.forEach((country) => {
+  axios
+    .get(country.url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-//         // Tag and HTML elements to fit the website
-//             $('li[role="listitem"]', html).each(function () {
-//               const title = $('h2', this).text();
-//               const id = $(this).attr('data-item-id');
-//               const alchool = $('div.NodeName_firstProperties__xGMas', this).text();
+      $('tr', html)
+        .has('td > b')
+        .each(function () {
+          const title = $('tr td', this).eq(0).text();
+          const alchool = $('tr td', this).eq(1).text();
+          const descriptionOne = $('tr td', this).eq(2).text();
+          const descriptionTwo = $('tr td', this).eq(3).text();
 
-//               beersList.push({ title, alchool, id, source: site.name });
-//             });
+          const description = descriptionOne.length > 6 && !descriptionOne.includes('º') ? descriptionOne : descriptionTwo;
 
-//   })
-// });
+          convertText(title);
 
-// app.get('/beers', (req,res) => {
-//   res.json(beerList)
-// })
+          if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
+            beersList.push({
+              title,
+              alchool,
+              description,
+              country: country.country,
+            });
+          }
+        });
+    })
+    .catch((err) => console.log(err));
+});
 
-//* INDIVIDUAL BEER BY PARAM */
+/* INDIVIDUAL BEER BY PARAM */
 
-// app.get('/beers/:beerId', (req, res) => {
-//   const beerId = req.params.beerId;
+app.get('/beers/:beerId', (req, res) => {
+  const beerId = req.params.beerId;
 
-//   const siteAddress = beerWebSites.filter((beerWebSite) => beerWebSite.name == beerId)[0].name;
+  const siteAddress = beers.filter((beer) => beer.country == beerId)[0].url;
 
-//   axios.get(siteAddress).then((response) => {
-//     const html = response.data;
-//     const $ = cheerio.load(html);
+  axios.get(siteAddress).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-//     const specificSite = [];
+    const specificSite = [];
 
-//     // Tag and HTML elements to fit the website
-//     $('li[role="listitem"]', html).each(function () {
-//       const title = $('h2', this).text();
-//       const id = $(this).attr('data-item-id');
-//       const alchool = $('div.NodeName_firstProperties__xGMas', this).text();
+    // Tag and HTML elements to fit the website
+    $('tr', html)
+      .has('td > b')
+      .each(function () {
+        const title = $('tr td', this).eq(0).text();
+        const alchool = $('tr td', this).eq(1).text();
+        const descriptionOne = $('tr td', this).eq(2).text();
+        const descriptionTwo = $('tr td', this).eq(3).text();
 
-//       specificSite.push({
-//         title,
-//         alchool,
-//         id,
-//         source: site.name,
-//       });
-//     });
-//     res.json(specificSite);
-//   });
-// });
+        const description = descriptionOne.length > 6 && !descriptionOne.includes('º') ? descriptionOne : descriptionTwo;
+
+        convertText(title);
+
+        if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
+          specificSite.push({
+            title,
+            alchool,
+            description,
+            country: beers.country,
+          });
+        }
+      });
+    res.json(specificSite);
+  });
+});
+
+app.get('/beers', (req, res) => {
+  const text = 'øøææåååasdø asdåøewr´ ør ñ æøløløp';
+  console.log(convertText(text));
+
+  res.json(beersList);
+});
 
 app.get('/', (req, res) => {
   res.json('Welcome to my Beer API');
@@ -99,134 +130,6 @@ app.get('/', (req, res) => {
 //   });
 // });
 
-app.get('/denmark', (req, res) => {
-  axios
-    .get('http://www.europeanbeerguide.net/denbrew.htm')
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('tr', html)
-        .has('td > b')
-        .each(function () {
-          const title = $('tr td', this).eq(0).text();
-          const alchool = $('tr td', this).eq(1).text();
-          const descriptionOne = $('tr td', this).eq(2).text();
-          const descriptionTwo = $('tr td', this).eq(3).text();
-
-          const description = descriptionOne.length > 2 ? descriptionOne : descriptionTwo;
-
-          convertText(title);
-
-          if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
-            beersListEBG.push({
-              title,
-              alchool,
-              description,
-            });
-          }
-        });
-      res.json(beersListEBG);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get('/sweden', (req, res) => {
-  axios
-    .get('http://www.europeanbeerguide.net/swedbrew.htm')
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('tr', html)
-        .has('td > b')
-        .each(function () {
-          const title = $('tr td', this).eq(0).text();
-          const alchool = $('tr td', this).eq(1).text();
-          const descriptionOne = $('tr td', this).eq(2).text();
-          const descriptionTwo = $('tr td', this).eq(3).text();
-
-          const description = descriptionOne.length > 2 ? descriptionOne : descriptionTwo;
-
-          convertText(title);
-
-          if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
-            beersListEBG.push({
-              title,
-              alchool,
-              description,
-            });
-          }
-        });
-      res.json(beersListEBG);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get('/belgium', (req, res) => {
-  axios
-    .get('http://www.europeanbeerguide.net/belgbrew.htm')
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('tr', html)
-        .has('td > b')
-        .each(function () {
-          const title = $('tr td', this).eq(0).text();
-          const alchool = $('tr td', this).eq(1).text();
-          const descriptionOne = $('tr td', this).eq(2).text();
-          const descriptionTwo = $('tr td', this).eq(3).text();
-
-          const description = descriptionOne.length > 2 ? descriptionOne : descriptionTwo;
-
-          convertText(title);
-
-          if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
-            beersListEBG.push({
-              title,
-              alchool,
-              description,
-            });
-          }
-        });
-      res.json(beersListEBG);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get('/spain', (req, res) => {
-  axios
-    .get('http://www.europeanbeerguide.net/spanbrew.htm')
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('tr', html)
-        .has('td > b')
-        .each(function () {
-          const title = $('tr td', this).eq(0).text();
-          const alchool = $('tr td', this).eq(1).text();
-          const descriptionOne = $('tr td', this).eq(2).text();
-          const descriptionTwo = $('tr td', this).eq(3).text();
-
-          const description = descriptionOne.length > 2 ? descriptionOne : descriptionTwo;
-
-          convertText(title);
-
-          if (alchool.includes('%') && !alchool.includes('-') && convertText(description) != '') {
-            beersListEBG.push({
-              title,
-              alchool,
-              description,
-            });
-          }
-        });
-      res.json(beersListEBG);
-    })
-    .catch((err) => console.log(err));
-});
-
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
 });
@@ -236,19 +139,25 @@ function convertText(text) {
 
   [...text].forEach((e) => {
     if (e.includes('ø')) {
-      e = e.replace('ø', 'o');
+      e = e.replace(/ø/g, 'o');
     }
     if (e.includes('æ')) {
-      e = e.replace('æ', 'ae');
+      e = e.replace(/æ/g, 'ae');
     }
     if (e.includes('å')) {
-      e = e.replace('å', 'o');
+      e = e.replace(/å/g, 'o');
     }
     if (e.includes('ä')) {
       e = e.replace('ä', 'o');
     }
     if (e.includes('ü')) {
       e = e.replace('ü', 'u');
+    }
+    if (e.includes('ñ')) {
+      e = e.replace('ñ', 'n');
+    }
+    if (e.includes('´' || '`')) {
+      e = e.replace('´' || '`', "'");
     }
     convertedText.push(e);
   });
